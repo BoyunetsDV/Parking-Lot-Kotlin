835dbc52039195ce6bed418769f1bf2d8482e33d
Boiunets Denys <boyunets.dv@gmail.com>
2020-11-04 23:42:03 +0200

    parking lot project completed

--- Parking Lot/task/src/parking/Main.kt
+++ Parking Lot/task/src/parking/Main.kt
@@ -10,7 +10,7 @@ fun main() {
     } while (command != "exit")
 }
 
-class Car(val number: String, val color: String)
+data class Car(val number: String, val color: String)
 
 object Parking {
     private var parkingSlots = emptyArray<Car?>()
@@ -26,6 +26,9 @@ object Parking {
             "park" -> parkCar(commandParts[1], commandParts[2])
             "leave" -> freeSpot(commandParts[1].toInt())
             "status" -> displayParkingStatus()
+            "reg_by_color" -> printAllRegistrationNumbersOfCarsByColor(commandParts[1])
+            "spot_by_color" -> printSpotsNumbersByCarColor(commandParts[1])
+            "spot_by_reg" -> printSpotNumberByCarNumber(commandParts[1])
         }
     }
 
@@ -77,10 +80,53 @@ object Parking {
         }
 
         for (i in parkingSlots.indices) {
-            if (parkingSlots[i] == null){
+            if (parkingSlots[i] == null) {
                 continue
             }
             println("${i + 1} ${parkingSlots[i]?.number} ${parkingSlots[i]?.color}")
+        }
+    }
+
+    private fun printAllRegistrationNumbersOfCarsByColor(color: String) {
+        if (isParkingIsNotCreated()) {
+            return
+        }
+
+        val cars = parkingSlots.filter { it?.color?.toLowerCase() == color.toLowerCase() }
+        if (cars.isEmpty()) {
+            println("No cars with color $color were found.")
+        } else {
+            println(cars.map { it?.number }.joinToString(", "))
+        }
+    }
+
+    private fun printSpotsNumbersByCarColor(color: String) {
+        if (isParkingIsNotCreated()) {
+            return
+        }
+        val spotIndexes = mutableListOf<Int>()
+        for (i in parkingSlots.indices) {
+            if (parkingSlots[i]?.color?.toLowerCase() == color.toLowerCase()) {
+                spotIndexes.add(i + 1)
+            }
+        }
+        if (spotIndexes.isEmpty()) {
+            println("No cars with color $color were found.")
+        } else {
+            println(spotIndexes.joinToString(", "))
+        }
+    }
+
+    private fun printSpotNumberByCarNumber(number: String) {
+        if (isParkingIsNotCreated()) {
+            return
+        }
+
+        val spotIndexes = parkingSlots.filter { it?.number == number }
+        if (spotIndexes.isEmpty()) {
+            println("No cars with registration number $number were found.")
+        } else {
+            println(parkingSlots.indexOf(spotIndexes[0]) + 1)
         }
     }
 
